0b40077588e38d7bc80b8dceab67d869a320dd13
Hubert Yu <hubert12161999@gmail.com>
2022-12-15 14:38:53 +0000

    Add basic calculator layout

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(operator, a, b) {
+  let answer;
+  switch (operator) {
+    case '+':
+      answer = add(a, b);
+      break;
+    case '-':
+      answer = subtract(a, b);
+      break;
+    case '*':
+      answer = multiply(a, b);
+      break;
+    case '÷':
+      answer = divide(a, b);
+      break;
+    default:
+      break;
+  }
+  return answer;
+}
